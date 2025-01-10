58c1c900cf4941341b9846b29cf94e74ef855a47
Sareh <moghaddam_sareh@yahoo.com>
2025-01-10 19:41:26 +0100

    add static data and using props and put the color to stars for voteaverage

--- components/Home.js
+++ components/Home.js
@@ -5,6 +5,13 @@ import { Button, Popover } from 'antd';
 
 
 function Home() {
+  const moviesData = [
+    { title: 'Forrest Gump', poster: 'forrestgump.jpg', voteAverage: 9.2, voteCount: 22_705, overview: 'A man with a low IQ has accomplished great things in his life and been present during significant historic events—in each case.' },
+    { title: 'The Dark Knight', poster: 'thedarkknight.jpg', voteAverage: 8.5, voteCount: 27_547, overview: 'Batman raises the stakes in his war on crime and sets out to dismantle the remaining criminal organizations that plague the streets.' },
+    { title: 'Your name', poster: 'yourname.jpg', voteAverage: 8.5, voteCount: 8_691, overview: 'High schoolers Mitsuha and Taki are complete strangers living separate lives. But one night, they suddenly switch places.' },
+    { title: 'Iron Man', poster: 'ironman.jpg', voteAverage: 7.6, voteCount: 22_7726, overview: 'After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.' },
+    { title: 'Inception', poster: 'inception.jpg', voteAverage: 8.4, voteCount: 31_546, overview: 'Cobb, a skilled thief who commits corporate espionage by infiltrating the subconscious of his targets is offered a chance to regain his old life.' },
+  ];
   const content = (
     <div>
       <p>Movie 1</p>
@@ -13,10 +20,19 @@ function Home() {
       <p>Movie 4</p>
     </div>
   );
-  const movies=[];
-  for(let i=0; i<10; i++){
-    movies.push(<Movie key={i}/>)
-  }
+  const movies = [];
+  moviesData.map((movie, i) => {
+    movies.push(
+      <Movie
+        key={i}
+        name={movie.title}
+        poster={movie.poster}
+        voteAverage={movie.voteAverage}
+        voteCount={movie.voteCount}
+        description={movie.overview}
+      />
+    );
+  })
 
  
   return (
--- components/Movie.js
+++ components/Movie.js
@@ -1,17 +1,24 @@
 import styles from '../styles/Movie.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
-function Movie() {
+function Movie(props) {
+    console.log(props)
     const stars=[];
-    for(let i=0; i<10; i++){
-        stars.push(<FontAwesomeIcon icon={faStar} key={i} />);
-    }
+    for (let i = 0; i < 10; i++) {
+        if (i < props.voteAverage-1) {
+          stars.push(<FontAwesomeIcon key={i} icon={faStar} style={{color:"#f1c40f"}} />);
+        } else {
+          stars.push(<FontAwesomeIcon key={i} icon={faStar}  />);
+        }
+        
+      }
+    
     return (
         <div className={styles.card}>
-        <img className={styles.poster} src='./poster.jpg' alt='Poster'/>
-        <p className={styles.name}>Name</p>
-        <p className={styles.description}>Description</p>
-        <p className={styles.vote}>{stars} (Vote count)</p>
+        <img className={styles.poster} src={props.poster} alt={props.name}/>
+        <p className={styles.name}>{props.name}</p>
+        <p className={styles.description}>{props.description}</p>
+        <p className={styles.vote}>{stars} ({props.voteCount})</p>
       </div>
     )
 }
