d47501df938ba6d5e306078bfbb9c9625cd9611d
Sareh <moghaddam_sareh@yahoo.com>
2025-01-22 21:50:54 +0100

    Dynamic like and rating system based on Star clicks

--- components/Movie.js
+++ components/Movie.js
@@ -1,7 +1,11 @@
 import styles from '../styles/Movie.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar, faHeart, faVideo } from '@fortawesome/free-solid-svg-icons';
+import {useState} from 'react'
 function Movie(props) {
+  const [isLiked, setIsLiked] = useState(false);
+  const [personalNote, setPersonalNote] = useState(0);
+  const [watchCount, setWatchCount] = useState(0);
     console.log(props)
     const stars=[];
     for (let i = 0; i < 10; i++) {
@@ -12,6 +16,20 @@ function Movie(props) {
         }
         
       }
+    const personalNotes=[];
+    for (let i = 0; i < 10; i++) {
+      let style={}
+        if(i < personalNote){
+          style={color:'#2196f3'}
+        }
+        personalNotes.push(<FontAwesomeIcon key={i} icon={faStar} onClick={()=>setPersonalNote(i+1)} style={style}/>);
+    }
+    const HandleWatchMovie=()=>{
+      setWatchCount(watchCount+1)
+    }
+    const HandleLikeMovie=()=>{
+      setIsLiked(!isLiked)
+    }
     
     return (
         <div className={styles.card}>
@@ -19,6 +37,28 @@ function Movie(props) {
         <p className={styles.name}>{props.name}</p>
         <p className={styles.description}>{props.description}</p>
         <p className={styles.vote}>{stars} ({props.voteCount})</p>
+        <p className={styles.vote}>{personalNotes} ({personalNote})</p>
+        <p className={styles.vote}>
+          <FontAwesomeIcon
+           icon={faVideo}
+           onClick={HandleWatchMovie}
+           style={ {
+            color: watchCount > 0 ? '#e74c3c' : '#000', 
+          }}
+           />
+          ({watchCount})
+        </p>
+        <p className={styles.vote}>
+          <FontAwesomeIcon
+           icon={faHeart}
+           onClick={HandleLikeMovie}
+           style={ {
+            color: isLiked ? '#e74c3c' : '#000', 
+          }}
+           />
+         
+        </p>
+       
       </div>
     )
 }
